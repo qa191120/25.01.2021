7d9d202c3643195ce359ebb6e9cb503deab053e0
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-01-29 07:20:51 +0200

    Update Circle.java

--- hw_solution/Circle.java
+++ hw_solution/Circle.java
@@ -8,6 +8,7 @@ public class Circle {
         //System.out.println("circle size");
         //         Math.PI * Math.pow(radius, 2);
         System.out.println(String.format("circle size %f", Math.PI * Math.pow(radius, 2)));
+        System.out.println(String.format("Area of Circle (r = %.1f)\n r^2*π = %.2f", radius, Math.pow(radius,2)*Math.PI))
 
     }
 }
